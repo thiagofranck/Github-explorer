7e3f152e9da9719093119973edf456a0626a83a0
Thiago Henrique <thiagohenrique776@gmail.com>
2021-03-16 17:36:57 -0300

    Sass/Components

---
+++ src/components/Counter.jsx
@@ -0,0 +1,18 @@
+import { userState, useState } from 'react';
+
+export function Counter() {
+    let counter = useState(0);
+
+    function increment() {
+        counter += 1;
+    }
+
+    return (
+        <div>
+            <h2>{counter}</h2>
+            <button type="button" onClick={increment}>
+                Increment
+            </button>
+        </div>
+    );
+}
